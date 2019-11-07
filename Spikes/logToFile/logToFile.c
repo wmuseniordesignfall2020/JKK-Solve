10e7681b35ec4000fb103f39887dccbc526edb56
ibrahimitani0 <jys5384@wmich.edu>
2019-11-06 20:51:36 -0500

    did spikes for logtofile

--- Spikes/logToFile/logToFile.c
+++ Spikes/logToFile/logToFile.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-	logToFile("Testing logging to a text file in Spikes");
-}
-
-int logToFile(char* message) {
-	FILE* fp;
-
-	if ((fp = fopen("logToFile.txt", "a")) == null){
-		return -1;
-    }
-	fprintf(fp, "%s\n", message);
-	fclose(fp);
+int main () {
+   FILE * fp;
+   int i;
+  
+   fp = fopen ("filelog.txt","a");
+ 
+   /* write 10 lines of text into the file stream*/
+   for(i = 0; i < 10;i++){
+       fprintf (fp, "This is line %d\n", i + 1);
+   }
+ 
+   
+   fclose (fp);
+   return 0;
 }
