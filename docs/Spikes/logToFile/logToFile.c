4e16e891f608490c1e1755e462c7ca4c42295e84
spencer <56516484+wmuseniordesignfall2020@users.noreply.github.com>
2019-11-06 21:45:50 -0500

    Moving Your Spikes Again
    Ibrahim Pull a Copy Real Quick and cd to docs/spikes I moved the spikes

--- docs/Spikes/logToFile/logToFile.c
+++ docs/Spikes/logToFile/logToFile.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <string.h>
 
 int main () {
    FILE * fp;
    int i;
-  
+   
    fp = fopen ("filelog.txt","a");
  
    /* write 10 lines of text into the file stream*/
    for(i = 0; i < 10;i++){
-       fprintf (fp, "This is line %d\n", i + 1);
+       fprintf (fp, "This is line %d\n",i + 1);
    }
  
-   
+   /* close the file*/  
    fclose (fp);
    return 0;
 }
